f1b1bd998e5cce239f5e80399f3cf4366b5a9684
anneaudistyaf <anneaudistyaf@gmail.com>
2021-05-14 17:41:50 +0700

    add menu

---
+++ Assets/Script/Information.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Information : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("START MENU");
+    }
+}
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HOW TO PLAY");
+    }
+    public void DetailLevel()
+    {
+        SceneManager.LoadScene("DETAIL LEVEL");
+    }
+    public void Credit()
+    {
+        SceneManager.LoadScene("CREDIT");
+    }
+    public void Information()
+    {
+        SceneManager.LoadScene("INFORMATION");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
